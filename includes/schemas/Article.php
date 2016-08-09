83e51a22ef5e9ec913a42a666f10bb31e12d0568
Steve Grunwell <stevegrunwell@gmail.com>
2016-08-08 21:55:53 -0400

    Add Article.wordCount

--- includes/schemas/Article.php
+++ includes/schemas/Article.php
@@ -23,4 +23,18 @@ class Article extends CreativeWork {
 		'pagination',
 		'wordCount',
 	);
+
+	/**
+	 * Retrieve the word-count of the post's post_content.
+	 *
+	 * @link http://www.thomashardy.me.uk/wordpress-word-count-function
+	 *
+	 * @param int $post_id The post ID.
+	 * @return int The number of words in the post.
+	 */
+	public function getWordCount( $post_id ) {
+		$content = get_post_field( 'post_content', $post_id );
+
+		return str_word_count( strip_tags( $content ) );
+	}
 }
