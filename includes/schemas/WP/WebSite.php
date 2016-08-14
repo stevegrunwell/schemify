4202706c0c34158df32edb82ab0da74984df6a92
Steve Grunwell <stevegrunwell@gmail.com>
2016-08-14 13:24:11 -0400

    Remove some unused properties for WP\WebSite

--- includes/schemas/WP/WebSite.php
+++ includes/schemas/WP/WebSite.php
@@ -13,4 +13,34 @@ use Schemify\Schemas as Schemas;
 class WebSite extends Schemas\WebSite {
 
 	use Traits\Schema, Traits\Blog;
+
+	/**
+	 * Remove the "author" node for WebSite.
+	 *
+	 * @param int $blog_id The blog ID. This value isn't used, but is preserved for compatibility
+	 *                     with CreativeWork::getAuthor().
+	 */
+	public function getAuthor( $blog_id ) {
+		return null;
+	}
+
+	/**
+	 * Remove the "publisher" node for WebSite.
+	 *
+	 * @param int $blog_id The blog ID. This value isn't used, but is preserved for compatibility
+	 *                     with CreativeWork::getPublisher().
+	 */
+	public function getPublisher( $blog_id ) {
+		return null;
+	}
+
+	/**
+	 * Remove the "thumbnailUrl" node for WebSite.
+	 *
+	 * @param int $blog_id The blog ID. This value isn't used, but is preserved for compatibility
+	 *                     with CreativeWork::getPublisher().
+	 */
+	public function getThumbnailUrl( $blog_id ) {
+		return null;
+	}
 }
