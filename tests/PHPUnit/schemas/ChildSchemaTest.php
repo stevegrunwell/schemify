a873d0ff5ebaa0f6c50d77ecc67e492eb18aefbc
Steve Grunwell <stevegrunwell@gmail.com>
2016-08-15 19:28:38 -0400

    To cut down on the number of overridden methods that just return NULL, add the $removeProperties array that automatically removes would-be inherited properties from the property list.

--- tests/PHPUnit/schemas/ChildSchemaTest.php
+++ tests/PHPUnit/schemas/ChildSchemaTest.php
@@ -39,4 +39,25 @@ class ChildSchemaTest extends Schemify\TestCase {
 			}
 		}
 	}
+
+	public function testGetPropertyListRemovesValues() {
+		require_once ABSPATH . 'TestChildSchema.php';
+
+		$instance          = new TestChildSchema( 123 );
+		$parent_reflection = new ReflectionClass( __NAMESPACE__ . '\Thing' );
+		$property          = new ReflectionProperty( $instance, 'removeProperties' );
+		$property->setAccessible( true );
+		$property->setValue( null, array( 'name' ) );
+		$method            = new ReflectionMethod( $instance, 'getPropertyList' );
+		$method->setAccessible( true );
+
+		// Verify that the parent schema has "name" to begin with.
+		$this->assertContains( 'name', $parent_reflection->getDefaultProperties()['properties'] );
+
+		// If this works, 'name' should be removed from the list.
+		$this->assertNotContains( 'name', $method->invoke( $instance ) );
+
+		// Important: reset the static TestChildSchema::$removeProperties!
+		$property->setValue( null, array() );
+	}
 }
